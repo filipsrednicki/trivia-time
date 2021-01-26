79e3ffadf2ad2bd44fdf9a29f3556ad4daa4a2f7
Filip <filipsrednicki42@gmail.com>
2021-01-26 23:32:06 +0100

    Created: user action creators

---
+++ src/actions/UserActions.ts
@@ -0,0 +1,41 @@
+import {
+  USER_LOADING,
+  AUTH_FAIL,
+  LOG_IN,
+  LOG_OUT,
+  SIGN_UP,
+  UserType,
+} from "./UserActionTypes";
+
+export const userLoading = () => {
+  return {
+    type: USER_LOADING,
+  } as const;
+};
+
+export const signUpUser = (user: UserType) => {
+  return {
+    type: SIGN_UP,
+    payload: user,
+  } as const;
+};
+
+export const logInUser = (user: UserType) => {
+  return {
+    type: LOG_IN,
+    payload: user,
+  } as const;
+};
+
+export const logOutUser = () => {
+  return {
+    type: LOG_OUT,
+  } as const;
+};
+
+export const authFail = (errorMsg: string) => {
+  return {
+    type: AUTH_FAIL,
+    payload: errorMsg
+  } as const;
+};
