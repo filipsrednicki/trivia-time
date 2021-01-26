b43d425459b5cca229b6bfadf807d51c61139afc
Filip <filipsrednicki42@gmail.com>
2021-01-26 23:26:29 +0100

    Created: UserActionTypes

---
+++ src/actions/UserActionTypes.ts
@@ -0,0 +1,33 @@
+export const SIGN_UP = "SIGN_UP";
+export const LOG_IN = "LOG_IN";
+export const LOG_OUT = "LOG_OUT";
+export const USER_LOADING = "USER_LOADING";
+export const AUTH_FAIL = "AUTH_FAIL";
+
+export type UserType = {
+  email: string;
+};
+
+export interface AuthLoading {
+  type: typeof USER_LOADING;
+}
+
+export interface AuthFail {
+  type: typeof AUTH_FAIL;
+}
+
+export interface SignUp {
+  type: typeof SIGN_UP;
+  payload: UserType;
+}
+
+export interface LogIn {
+  type: typeof LOG_IN;
+  payload: UserType;
+}
+
+export interface LogOut {
+  type: typeof LOG_OUT;
+}
+
+export type UserDispatchTypes = AuthLoading | AuthFail | SignUp | LogIn | LogOut;
