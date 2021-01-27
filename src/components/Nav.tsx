18c1f1f7e7e189dde2071f36654a24da5d03a8c1
Filip <filipsrednicki42@gmail.com>
2021-01-27 23:52:01 +0100

    Added: navbar component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootStore } from "../store";
+import { logOut } from "../actions/userActions";
+import Modal from "./Modal";
+import Auth from "./Auth";
+
+const Nav: React.FC = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const { user } = useSelector((state: RootStore) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) {
+      setShowModal(false);
+    }
+  }, [user]);
+
+  return (
+    <nav>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/trivia-sets">Trivia Sets</Link>
+        {user && <Link to="/create-trivia">Create Trivia Set</Link>}
+      </div>
+      <div className="nav-btns">
+        {!user ? (
+          <button onClick={() => setShowModal(true)}>Log In</button>
+        ) : (
+          <button onClick={() => dispatch(logOut())}>Log Out</button>
+        )}
+      </div>
+      <Modal
+        closeModal={() => setShowModal(false)}
+        isShown={showModal}
+        closeButton={true}
+      >
+        <Auth />
+      </Modal>
+    </nav>
+  );
+};
+
+export default Nav;
