abb9b94ed31b7cbec7bd469f20488400b743247f
Filip <filipsrednicki42@gmail.com>
2021-01-28 19:56:57 +0100

    Added: checking username validity when signing up

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { logIn, signUp } from "../actions/userActions";
+import { database } from "../firebase";
 import InputEl from "./InputEl";
 
 const Auth: React.FC = () => {
@@ -10,10 +11,36 @@ const Auth: React.FC = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isSignUpMode, setIsSignUpMode] = useState(false);
+  const [isUsernameInvalid, setIsUsernameInvalid] = useState(false);
+  const [isUsernameTaken, setIsUsernameTaken] = useState(false);
+
+  const handleUsernameChange = (name: string) => {
+    setUsername(name);
+    setIsUsernameInvalid(false);
+    setIsUsernameTaken(false);
+    if (name.length > 1 && !name.match(/^[a-z0-9_]{2,20}$/i)) {
+      return setIsUsernameInvalid(true);
+    }
+    database
+      .ref("usernames")
+      .once("value")
+      .then((snapshot) => {
+        const usernameList: string[] = Object.values(snapshot.val());
+        if (usernameList.includes(name)) {
+          setIsUsernameTaken(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (isSignUpMode) {
+      if (isUsernameTaken) {
+        return;
+      }
       dispatch(signUp(email, username, password));
     } else {
       dispatch(logIn(email, password));
@@ -23,16 +50,34 @@ const Auth: React.FC = () => {
   return (
     <div className="auth">
       <form onSubmit={handleSubmit}>
+        <h1>
+          {isSignUpMode ? "Create an account" : "Log In"}
+        </h1>
         {isSignUpMode && (
-          <InputEl
-            id="username"
-            label="Username"
-            type="text"
-            value={username}
-            setValue={setUsername}
-            minLength={2}
-            maxLength={20}
-          />
+          <>
+            <InputEl
+              id="username"
+              label="Username"
+              type="text"
+              value={username}
+              setValue={handleUsernameChange}
+              minLength={2}
+              maxLength={20}
+              {...((isUsernameInvalid || isUsernameTaken) && {
+                className: "invalid-input",
+              })}
+            />
+            {isUsernameInvalid && (
+              <span className="invalid-msg">
+                Username can't contain special characters other than "_".
+              </span>
+            )}
+            {isUsernameTaken && (
+              <span className="invalid-msg">
+                Sorry, this username in unavailable.
+              </span>
+            )}
+          </>
         )}
         <InputEl
           label="E-mail"
@@ -61,7 +106,7 @@ const Auth: React.FC = () => {
         )}
         <button type="submit">{isSignUpMode ? "Sign Up" : "Log In"}</button>
 
-        <span onClick={() => setIsSignUpMode((bool) => !bool)}>
+        <span className="auth-mode-btn" onClick={() => setIsSignUpMode((bool) => !bool)}>
           {isSignUpMode
             ? "Already have an account? Log In!"
             : "Need an account? Create one!"}
