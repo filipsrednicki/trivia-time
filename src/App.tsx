ad3625cbda1b93ffd604c8ee3ffd11d135a9bd2c
Filip <filipsrednicki42@gmail.com>
2021-01-31 21:52:18 +0100

    Added: auto-login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import "./App.scss";
 import ConfigureTriviaSet from "./components/TriviaSet/ConfigureTriviaSet";
 import CreateTriviaSet from "./components/TriviaSet/CreateTriviaSet";
 import Nav from "./components/Nav";
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
+import { logInUser, userLoading } from "./actions/userActions";
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(userLoading());
+    auth.onAuthStateChanged((user) => {
+      if (user?.email) {
+        dispatch(logInUser({ email: user.email, username: user.displayName }));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Nav/>
