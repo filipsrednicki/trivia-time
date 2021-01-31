872fb3451aab7061ea88122c4b45ce5fe6c68f8e
Filip <filipsrednicki42@gmail.com>
2021-01-31 18:32:26 +0100

    Added: triviaReducer to the rootReducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import triviaReducer from "./triviaReducer";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  trivias: triviaReducer
 })
 
 export default rootReducer;
