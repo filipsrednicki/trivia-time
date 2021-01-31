a0e7a2b32417e20851a363f75251532876526d1b
Filip <filipsrednicki42@gmail.com>
2021-01-31 18:40:35 +0100

    Added: Route for CreateTriviaSet component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.scss";
+import CreateTriviaSet from "./components/TriviaSet/CreateTriviaSet";
 import Nav from "./components/Nav";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Nav/>
+      <Route path="/create-set" render={({ match: { url }}) => (
+        <>
+          <Route exact path={`${url}/`} component={CreateTriviaSet}/>
+        </>
+      )}/>
     </div>
   );
 };
