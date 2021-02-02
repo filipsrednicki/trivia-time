57930b30a3e03d491be606d528d5316c5a3e5289
Filip <filipsrednicki42@gmail.com>
2021-02-02 23:51:09 +0100

    Added: interface for TriviaSet

--- src/actions/triviaActionTypes.ts
+++ src/actions/triviaActionTypes.ts
@@ -1,6 +1,7 @@
 export const ADD_TRIVIA = "ADD_TRIVIA";
 export const REMOVE_TRIVIA = "REMOVE_TRIVIA";
 export const CLEAR_TRIVIA = "CLEAR_TRIVIA";
+export const SET_FETCHED_SETS = "SET_FETCHED_SETS";
 
 export interface Trivia {
   category: string;
@@ -10,6 +11,30 @@ export interface Trivia {
   incorrect_answers: string[];
   question: string;
   type: string;
+}
+
+interface TopPlayer {
+  score: number;
+  name: string;
+}
+
+interface FiftyFifty {
+  isAllowed: boolean;
+  penalty: number;
+}
+
+export interface TriviaSet {
+  name: string;
+  id: string;
+  public: boolean;
+  date: string;
+  rating: number[];
+  timePerQuestion: number;
+  maxPoints: number;
+  creator: string | undefined | null;
+  fiftyFifty: FiftyFifty;
+  leaderboard?: TopPlayer[];
+  trivias: Trivia[];
 }
 
 export interface AddTrivia {
@@ -26,4 +51,9 @@ export interface ClearTrivia {
   type: typeof CLEAR_TRIVIA;
 }
 
-export type TriviaDispatchTypes = AddTrivia | RemoveTrivia | ClearTrivia;
+export interface SetFetchedTriviaSets {
+  type: typeof SET_FETCHED_SETS;
+  payload: TriviaSet[];
+}
+
+export type TriviaDispatchTypes = AddTrivia | RemoveTrivia | ClearTrivia | SetFetchedTriviaSets;
