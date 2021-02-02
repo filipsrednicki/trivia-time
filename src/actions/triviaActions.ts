0b461bda45e7c2d5e5de980f1bc7edb357f3c82f
Filip <filipsrednicki42@gmail.com>
2021-02-02 23:51:56 +0100

    Added: actions for fetching Trivia Sets

--- src/actions/triviaActions.ts
+++ src/actions/triviaActions.ts
@@ -1,4 +1,6 @@
-import { ADD_TRIVIA, REMOVE_TRIVIA, Trivia } from "./triviaActionTypes";
+import { Dispatch } from "redux";
+import { database } from "../firebase";
+import { ADD_TRIVIA, REMOVE_TRIVIA, SET_FETCHED_SETS, Trivia, TriviaSet } from "./triviaActionTypes";
 
 export const addTrivia = (trivia: Trivia) => {
   return {
@@ -12,4 +14,33 @@ export const removeTrivia = (id: string) => {
     type: REMOVE_TRIVIA,
     payload: id,
   };
+};
+
+export const setFetchedTriviaSets = (triviaSets: TriviaSet[]) => {
+  return {
+    type: SET_FETCHED_SETS,
+    payload: triviaSets,
+  };
+};
+
+interface KeyValuePair {
+  key: string;
+  value: TriviaSet;
+}
+
+export const getStoredSets = () => (
+  dispatch: Dispatch
+) => {
+  database.ref("/triviaSets/").get().then((snapshot) => {
+    const result: KeyValuePair = snapshot.val();
+    const resultArr: TriviaSet[] = [];
+    for (const [key, value] of Object.entries(result)) {
+      value.id = key;
+      resultArr.push(value);
+    }
+    dispatch(setFetchedTriviaSets(resultArr));
+  })
+  .catch(err => {
+    console.log(err);
+  })
 };
